7e94c542893beef4721783cc91273b79669990b0
ppruektha <phaisit@126.com>
2024-08-25 06:03:03 -0400

    Task 6

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ def sent_analyzer():
 
 @app.route("/")
 def render_index_page():
-    return render_template('oaqjp-final-project-emb-ai/templates/index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
