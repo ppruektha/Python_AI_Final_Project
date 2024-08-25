0ba45f536a3f2d2e1d8538924f9e1c6f935ec784
ppruektha <phaisit@126.com>
2024-08-25 05:55:03 -0400

    Task 6

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotional Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    text_to_analyze = request.args.get("textToAnalyze")
+
+    response = sent_analyzer(text_to_analyze)
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant_emotion)
+
+@app.route("/")
+def render_index_page():
+    return render_template('oaqjp-final-project-emb-ai/templates/index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
